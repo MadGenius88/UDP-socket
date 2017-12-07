c482a9fec37eae7823f672caf9b7959ca30cb6a5
Calvin Secrest <csecrest2@gmail.com>
2017-12-07 02:01:57 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,18 @@ import socket
 import sys
 import time
 
-HOST = "localhost"
-PORT = "5454"
-data = "It always seems impossible until it's done - Nelson Mandela"
-
 # Set up our sending socket UDP
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, # Internet
+                    socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  print "received message:", data
+        
 class server
         s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         s.bind( (HOST,PORT) )
