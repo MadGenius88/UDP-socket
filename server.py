a4c92401c793d85bb5ecb06e25693282cb7c497e
Calvin Secrest <csecrest2@gmail.com>
2017-12-08 18:52:06 -0500

    Update server.py

--- server.py
+++ server.py
@@ -20,10 +20,37 @@ class server
         
 connections = []
 
-# Send greeting to the receiver
+# Send packet to the receiver
 def handler(c, a):
         while 2:
         s.sendto(data,(HOST<PORT))
         print "sent: "+data
         time.sleep(1)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind('127.0.0.1',4545)
+
+while 1:
+    packet = sock.recvfrom(65565)
+    packet = packet[0]
+
+    # parse IP
+    ip_header = packet[0:20]
+    iph = struct.unpack('!BBHHHBBH4s4s' , ip_header)
+
+    #all the following values are incorrect
+    version_ihl = iph[0]
+    version = version_ihl >> 4
+    ihl = version_ihl & 0xF
+
+    ttl = iph[5]
+    protocol = iph[6]
+    s_addr = socket.inet_ntoa(iph[8]);
+    d_addr = socket.inet_ntoa(iph[9]);
+
+    # parse UDP
+    packet = packet[20:28]
+    data = packet[header_length:]
+    source_port, dest_port, data_length, checksum = struct.unpack("!HHHH", header)
+
 
