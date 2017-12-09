da194241dd903c09d6057bda715f890812b51a46
Calvin Secrest <csecrest2@gmail.com>
2017-12-09 00:25:17 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,10 @@
 # Receiver
 import socket, sys, time
-from struct import *
+from struct threading *
 
 # Set up receiving socket UDP
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-MESSAGE = "It always seems impossible until it's done - Nelson Mandela"
 MAX_FAILS_SENDING = 10
 MTU_DISCOVERY_SEQ = 0 # the Sequence number sending MTU discovery messages
 TIMEOUT = 0.0002
@@ -13,7 +12,6 @@ BUFFER  = 64536
  
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
 
 # Elaspsed time 
    for pings in range(10):
@@ -37,12 +35,12 @@ sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
 sock.sendto(bytes(MESSAGE), (UDP_IP, UDP_PORT))
 
-def test_adler32start(self):
+def adler32start(self):
         self.assertEqual(zlib.adler32(""), zlib.adler32("", 1))
         self.assert_(zlib.adler32("abc", 0xffffffff))
 
 def checksum = sum(bytearray(b)"UTF-4")
-sock.sendto(bytes(MESSAGE, "UTF-4"), (UDP_IP, UDP_PORT))
+    sock.sendto(bytes(MESSAGE, "UTF-4"), (UDP_IP, UDP_PORT))
 
 # Receive and print data
 while 2:
@@ -54,10 +52,12 @@ while 2:
 # Payload size
 with open('payloadfile.bin') as op:
     payload = pickle.load(op)
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(MESSAGE, "UTF-8"), (UDP_IP, UDP_PORT))
+  
+def payload = sum(bytearray(b)"UTF-2")
+     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+     sock.sendto(bytes(MESSAGE, "UTF-8"), (UDP_IP, UDP_PORT))
 
 for i in payload:
     sock.sentto(payload, ('127.0.0.1',5005))
-  
+    print(data, type(data))
+
