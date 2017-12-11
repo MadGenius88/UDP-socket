81bb7c01d474dfb4ac87d1abe601c8d6af1c0c40
Calvin Secrest <csecrest2@gmail.com>
2017-12-10 23:36:25 -0500

    Create socket_client.py

---
+++ socket_client.py
@@ -0,0 +1,61 @@
+import socket
+import sys
+
+
+class Socket_Client:
+
+    def __init__(self,type, port):
+        self.server_address = (socket.gethostname(), port)
+        if type.upper() == "TCP":
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect(self.server_address)
+        else:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+    def get_udp(self,message):
+        self.sock.sendto(message.encode(encoding='utf_8'),self.server_address)
+        data, address = self.sock.recvfrom(1024)
+        self.sock.close()
+        return data.decode()
+
+    def get_tcp(self,message):
+        self.sock.sendall(message.encode(encoding='utf_8'))
+        response = self.sock.recv(1024).decode()
+        self.sock.close()
+        return response
+
+    def send_file_udp(self,message,filename):
+        self.sock.sendto(message.encode(encoding='utf_8'), self.server_address)
+        data, address = self.sock.recvfrom(1024)
+        print(data)
+        # buf = 1024
+        # self.sock.sendto(filename.encode(encoding='utf_8'), self.server_address)
+        # f = open(filename, "rb")
+        # data = f.read(buf)
+        # while (data):
+        #     if (self.sock.sendto(data, self.server_address)):
+        #         data = f.read(buf)
+        # self.sock.close()
+        # f.close()
+
+    def manage_client_requests(self):
+        try:
+            # Send data
+            message = 'This is the message.  It will be repeated.'
+            print('sending "%s"' % message)
+            self.sock.sendall(message.encode(encoding='utf_8'))
+
+            # Look for the response
+            amount_received = 0
+            amount_expected = len(message)
+
+            while amount_received < amount_expected:
+                data = self.sock.recv(16)
+                amount_received += len(data)
+                print('received "%s"' % data)
+
+        finally:
+            print('closing socket')
+            self.sock.close()
+
+#newClient = Socket_Client('192.168.223.1',6868,"TCP")
